1a387e74ded30668ce1c79a88f820ed1a9fcc9ff
michael1yu <31612776+michael1yu@users.noreply.github.com>
2023-03-26 18:40:15 -0700

    Update conv_transformer_trainer.py

--- utils/conv_transformer_trainer.py
+++ utils/conv_transformer_trainer.py
@@ -54,7 +54,7 @@ class ConvTransformerTrainer:
         x = tf.keras.layers.Lambda(lambda x: tf.expand_dims(x, axis=-1))(inputs)
         print("CONV2D Shape: ", x.get_shape().as_list())
         x = tf.keras.layers.Conv2D(10, 2, activation='relu', input_shape=(*shape, 1))(x)
-        #x = tf.keras.layers.Lambda(lambda x: tf.squeeze(x))(x)
+        x = tf.keras.layers.Lambda(lambda x: tf.reshape(x, shape=shape))(x)
         print("Flattened: ", x.get_shape())
         x = tf.keras.layers.Dense(self.d_model)(x)
         print("Densed: ", x.get_shape())
